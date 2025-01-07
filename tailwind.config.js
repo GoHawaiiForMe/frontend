3b290e34c8f096f5e18ecae8519b7cec2127cead
gahyeon-yang <didrkgus0426@gmail.com>
2025-01-07 13:32:21 +0900

    fix: 미디어쿼리 조정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", "./src/features/**/*.{js,ts,jsx,tsx}", ],
   theme: {
     screens: {
-      mobile: { max: "374px" },
+      mobile: { max: "743px" },
       tablet: { min: "744px", max: "1023px" },
       pc: { min: "1024px" },
-      "mobile-tablet": { min: "375px", max: "1023px" },
+      "mobile-tablet": { max: "1023px" },
     },
     extend: {
       colors: {
