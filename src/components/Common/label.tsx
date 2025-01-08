941c7168bf49e664ff4c14f413ddfc67422995b1
junbaehyun <reasonmylife@naver.com>
2025-01-08 18:54:13 +0900

    feat:label compponent update

---
+++ src/components/Common/label.tsx
@@ -0,0 +1,4 @@
+import label_office from "../../assets/label_office.svg";
+import label_home from "../../assets/label_home.svg";
+import label_file_dock from "../../assets/label_file_dock.svg";
+import label_box from "../../assets/label_box.svg";
