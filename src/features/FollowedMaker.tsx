dc361a5e09898b60baad4d1c598df31c9390d942
Yang ga-hyeon <didrkgus0426@gmail.com>
2025-01-14 16:21:17 +0900

    fix: 컴포넌트 이름 변경

--- src/features/FollowedMaker.tsx
+++ src/features/FollowedMaker.tsx
@@ -1,4 +1,4 @@
-import SavedCard from "@/components/Common/SavedCard";
+import FollowedCard from "@/components/Common/FollowedCard";
 
 export default function SavedMaker() {
   return (
@@ -9,10 +9,10 @@ export default function SavedMaker() {
       <div className="h-0.5 pc:-mx-[260px] tablet:-mx-[72px] mobile:-mx-[24px] bg-color-line-200 mb-8"></div>
 
       <div className="card:flex card:flex-col pc:grid pc:grid-cols-2 mobile-tablet:flex mobile-tablet:flex-col gap-4 justify-center items-center ">
-        <SavedCard />
-        <SavedCard />
-        <SavedCard />
-        <SavedCard />
+        <FollowedCard />
+        <FollowedCard />
+        <FollowedCard />
+        <FollowedCard />
       </div>
     </>
   );
