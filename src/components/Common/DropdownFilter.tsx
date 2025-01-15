f3c6c74abe06b038de471058abef321de54d641b
junbaehyun <reasonmylife@naver.com>
2025-01-15 17:31:03 +0900

    feat: dropdown component update

--- src/components/Common/DropdownFilter.tsx
+++ src/components/Common/DropdownFilter.tsx
@@ -2,8 +2,17 @@ import React, { useState } from 'react';
 import downBlue from '@public/assets/dropdown_down_blue.svg';
 import downGray from '@public/assets/dropdown_down_gray.svg';
 
-const Dropdown: React.FC = () => {
+interface Props { 
+	placeholder: string
+	items: string[] 
+
+}
+
+const Dropdown: React.FC<Props> = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null); //선택된 초기 데이터 없음 
+
+
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -15,23 +24,16 @@ const Dropdown: React.FC = () => {
         onClick={toggleDropdown}
         className="shadow w-40"
       >
-        지역
+        {selectedItem || props.placeholder } 
       </button>
-      {isOpen && (
+      {isOpen && ( 
         <div className="absolute mt-1 bg-white shadow-lg z-10 flex" style={{ top: '100%' }}>
           <div className="flex-1 p-2">
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">전체</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">서울</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">제주도</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">인천</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">부산</a>
-          </div>
-          <div className="flex-1 p-2">
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">하와이</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">태국</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">베트남</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">호주</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">뉴질랜드</a>
+            
+           {props.items.map(item=>{
+           	return  <button onClick={() => setSelectedItem(item)}  className="block px-4 py-2 text-gray-700 hover:bg-gray-100">{item}</button>
+           	})}
+
           </div>
         </div>
       )}
