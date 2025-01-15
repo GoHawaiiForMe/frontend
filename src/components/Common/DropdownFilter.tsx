137b66bb7816a247a49e38d5c00d521d72026611
junbaehyun <reasonmylife@naver.com>
2025-01-15 13:59:47 +0900

    style: update doropdown style

--- src/components/Common/DropdownFilter.tsx
+++ src/components/Common/DropdownFilter.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import downBlue from '@public/assets/dropdown_down_blue.svg';
+import downGray from '@public/assets/dropdown_down_gray.svg';
 
 const Dropdown: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,28 +10,28 @@ const Dropdown: React.FC = () => {
   };
 
   return (
-    <div className="relative inline-block">
+    <div className="flex relative">
       <button
         onClick={toggleDropdown}
-        className="bg-blue-100 border border-blue-500 text-blue-700 py-2 px-4 rounded w-full text-left"
+        className="shadow w-40"
       >
         지역
       </button>
       {isOpen && (
-        <div className="absolute mt-1 w-full bg-white shadow-lg z-10 flex">
+        <div className="absolute mt-1 bg-white shadow-lg z-10 flex" style={{ top: '100%' }}>
           <div className="flex-1 p-2">
             <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">전체</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">경기</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">강원</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">충남</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">대전</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">서울</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">제주도</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">인천</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">부산</a>
           </div>
           <div className="flex-1 p-2">
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">서울</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">인천</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">충북</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">세종</a>
-            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">전북</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">하와이</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">태국</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">베트남</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">호주</a>
+            <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">뉴질랜드</a>
           </div>
         </div>
       )}
