e858009986406443fc42a188564e24d9c81efec4
junbaehyun <reasonmylife@naver.com>
2025-01-20 00:00:42 +0900

    style: update

--- src/components/Common/DropdownFilter.tsx
+++ src/components/Common/DropdownFilter.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import downBlue from '@public/assets/dropdown_down_blue.svg';
 import downGray from '@public/assets/dropdown_down_gray.svg';
-//기본 문자값 변수 정의, 
+
 const placeholder = '지역';
 const items = ['전체','서울','인천','부산','제주도', '하와이', '태국', '베트남','호주', '뉴질랜드' ]
 
 const placeholder2 = '서비스';
-const items2 = ['액티비티형','식도락형','쇼핑형','문화형','휴식형','요청형']
+const items2 = ['기념품/쇼핑형', '맛집 탐방형', '액티비티/탐험형', '문화/역사탐방형', '축제참여형', '휴양형'];
 
-// 받아서 처리할 것  -> 범용 컴포넌트 만들기 위해 
+
 
 interface DropdownProps {
   type: 'location' | 'service';
@@ -26,28 +26,29 @@ const Dropdown: React.FC<DropdownProps> = ({ type }) => {
   const placeholderText = type === 'location' ? placeholder : placeholder2;
 
   return (
-    <div className="flex relative">
+    <div className="w-full relative mt-4">
       <button
         onClick={toggleDropdown}
-        className="shadow w-40"
+        className={`w-[328px] h-16 font-[18px] text-color-black-400 px-4 flex justify-between items-center border rounded-[16px] bg-white cursor-pointer transition duration-200 shadow-sm 
+          ${isOpen ? 'border-color-blue-300 bg-color-blue-50 shadow-md' : ''} 
+          ${selectedItem ? 'border-color-blue-300' : 'border-color-line-200'} `}
       >
-        {/* 지역 //지역이 아닌 선택된 데이터로 보여지게 , 지역선택 -> 지역 or 선택x ->지역 ,  // 앞에가 true -> 앞에 렌더링  if not region  command D 
-        데이터를 받아서 사용할 수 있게 하는 것  , 지역과 서비스를 받아서 사용할 수 있게 하는 것 make available whatever data you get 
-        1. without data what would be = region  기본 설정 문자값 
-        2. 실제 선택할 수 있는 데이터들 
-         받아서 처리
-          //데이터가 있어서 데이터를 가지고 그 데이터를 통해서 버튼을 만들것
-         */}
-        {selectedItem || placeholderText}
+        <span className={`text-[18px] font-medium text-color-black-400 ${selectedItem ? 'text-color-blue-300' : ''}`}>
+          {selectedItem || placeholderText}
+        </span>
+        <img src={isOpen ? downBlue : downGray} alt="dropdown icon" className="w-4 h-4" />
       </button>
       {isOpen && (
-        <div className="absolute mt-1 bg-white shadow-lg z-10 flex" style={{ top: '100%' }}>
-          <div className="flex-1 p-2">
+        <div className={`w-[328px] h-80 absolute top-full left-0 bg-white border border-gray-300 rounded-[16px] z-10 shadow-sm transition duration-200 ${isOpen ? 'z-50 shadow-md' : ''}`}>
+          <div className={`${type === 'location' ? 'grid grid-cols-2  overflow-y-auto mt-2' : 'flex flex-col w-28 h-36 overflow-y-hidden mt-2'}`}>
             {itemsToDisplay.map(item => (
               <button
                 key={item}
-                onClick={() => setSelectedItem(item)}
-                className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                onClick={() => {
+                  setSelectedItem(item);
+                  setIsOpen(false);
+                }}
+                className={`px-3.5 py-1.5 text-[18px] font-medium text-color-black-400 cursor-pointer transition duration-200 hover:bg-gray-100 ${type === 'service' ? 'text-left' : ''}`}
               >
                 {item}
               </button>
