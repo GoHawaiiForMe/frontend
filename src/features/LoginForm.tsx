a72631f88ac6dcc22fa5183e4782986b5698eb12
Yang ga-hyeon <didrkgus0426@gmail.com>
2025-01-23 14:02:03 +0900

    feat: 로그인폼 리액트쿼리로 변경

--- src/features/LoginForm.tsx
+++ src/features/LoginForm.tsx
@@ -12,34 +12,50 @@ import kakao_icon from "@public/assets/icon_kakao.svg";
 import naver_icon from "@public/assets/icon_naver.svg";
 import userService from "@/services/userService";
 import { useRouter } from "next/router";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+interface LoginProps {
+  email: string;
+  password: string;
+}
+
+const getUserInfo = async () => {
+  const userData = await userService.getUserInfo();
+  return userData;
+};
+
+const postLogin = async (LoginData: LoginProps) => {
+  return userService.login(LoginData);
+};
 
 export default function LoginForm() {
   const router = useRouter();
   const { setLogin } = useAuthStore();
 
-  const handleLogin = async (data: LoginFormData) => {
-    try {
-      await userService.login(data);
-
-      // 유저 정보 요청
-      const userInfo = await userService.getUserInfo();
-      setLogin(userInfo.nickName, userInfo.role as "DREAMER" | "MAKER", userInfo.coconut);
-
-      router.push("/");
-
-    } catch (error: any) {
+  const mutation = useMutation({
+    mutationFn: postLogin,
+    onSuccess: async () => {
+      try {
+        const userInfo = await getUserInfo();
+        setLogin(userInfo.nickName, userInfo.role as "DREAMER" | "MAKER", userInfo.coconut);
+        router.push("/");
+      } catch (error) {
+        console.error("유저 정보 가져오기 실패", error);
+      }
+    },
+    onError: (error: any) => {
       if (error.response) {
-        console.error('Error response:', error.response);
+        console.error("Error response", error.response);
         if (error.response.status === 400) {
           alert("로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.");
         }
       } else if (error.request) {
-        console.error('Error request:', error.request);
+        console.error(error.request);
       } else {
-        console.error('Error message:', error.message);
+        console.error(error.message);
       }
-    }
-  };
+    },
+  });
 
   const {
     register,
@@ -52,21 +68,21 @@ export default function LoginForm() {
   });
 
   const onSubmit = (data: LoginFormData) => {
-    handleLogin(data);
+    mutation.mutate(data);
   };
 
   const watchFields = watch();
   const isFormValid = Object.values(watchFields).every((value) => value?.toString().trim() !== "");
 
   const ErrorMessage = ({ message }: { message: string | undefined }) => (
-    <p className="text-color-red-200 mt-1 absolute right-0">{message}</p>
+    <p className="absolute right-0 mt-1 text-color-red-200">{message}</p>
   );
 
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center gap-8 pc:w-[640px] mobile-tablet:w-[372px]">
         <Image src={logo} alt="로고" />
-        <form className="w-full gap-8 flex flex-col relative" onSubmit={handleSubmit(onSubmit)}>
+        <form className="relative flex w-full flex-col gap-8" onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-2">
             <Input
               {...register("email")}
@@ -88,22 +104,27 @@ export default function LoginForm() {
             {errors.password && <ErrorMessage message={errors.password.message} />}
           </div>
           <div className="flex justify-center">
-            <Button label="로그인" type="submit" disabled={!isFormValid} className="text-color-gray-50" />
+            <Button
+              label="로그인"
+              type="submit"
+              disabled={!isFormValid}
+              className="text-color-gray-50"
+            />
           </div>
         </form>
-        <div className="flex justify-center mb-10 pc:text-xl ">
+        <div className="mb-10 flex justify-center pc:text-xl">
           <p className="mr-2 text-lg mobile-tablet:text-xs">
             아직 니가가라하와이 회원이 아니신가요?
           </p>
           <Link
             href="/signup"
-            className="text-color-blue-300 underline text-lg mobile-tablet:text-xs"
+            className="text-lg text-color-blue-300 underline mobile-tablet:text-xs"
           >
             이메일로 회원가입하기
           </Link>
         </div>
         <div className="flex flex-col items-center gap-8">
-          <h2 className="pc:text-xl tablet:text-xs">SNS 계정으로 간편 가입하기</h2>
+          <h2 className="tablet:text-xs pc:text-xl">SNS 계정으로 간편 가입하기</h2>
           <div className="flex gap-4">
             <Image src={google_icon} alt="구글 아이콘" width={50} height={50} />
             <Image src={kakao_icon} alt="카카오 아이콘" width={50} height={50} />
