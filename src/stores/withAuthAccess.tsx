5464d9f76344e19dde10ebbd5497d7d384950cf5
Yang ga-hyeon <didrkgus0426@gmail.com>
2025-01-25 18:14:30 +0900

    withAuthAccess useAuthStore 삭제

--- src/stores/withAuthAccess.tsx
+++ src/stores/withAuthAccess.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import useAuthStore from "./useAuthStore";
 
 const withAuthAccess = (WrappedComponent: React.ComponentType) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
