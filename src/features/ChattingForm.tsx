1eeb6600449bd8fc19b47a49c1f27c1aa641f5de
Yang ga-hyeon <didrkgus0426@gmail.com>
2025-02-11 15:27:20 +0900

    feat: 채팅방 로딩중 표시

--- src/features/ChattingForm.tsx
+++ src/features/ChattingForm.tsx
@@ -11,6 +11,7 @@ import { formatToDetailedDate } from "@/utils/formatDate";
 import coconut from "@public/assets/icon_coconut.svg";
 import { Socket } from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
+import loading from "@public/assets/icon_loading.gif";
 
 const MAX_IMAGE_SIZE = 5 * 1024 * 1024;
 const MAX_VIDEO_SIZE = 100 * 1024 * 1024;
@@ -33,6 +34,7 @@ export default function ChattingForm() {
   const [isFetchingOldMessages, setIsFetchingOldMessages] = useState(false);
   const [isFirstMessage, setIsFirstMessage] = useState(false);
   const [hasMoreMessages, setHasMoreMessages] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
   const messagesContainerRef = useRef<HTMLDivElement | null>(null);
@@ -282,12 +284,15 @@ export default function ChattingForm() {
   };
 
   useEffect(() => {
+    setIsLoading(true);
     const getChatRooms = async () => {
       try {
         const data = await chatService.getChatRooms();
         setChatRooms(data);
       } catch (error) {
         console.error("채팅방을 가져오는데 실패했습니다.", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     getChatRooms();
@@ -408,7 +413,11 @@ export default function ChattingForm() {
         </div>
       );
     } else {
-      return <p>지원되지 않는 파일 형식입니다.</p>;
+      return (
+        <p>
+          지원되지 않는 파일 형식입니다. (이미지는 jpg, jpeg, png / 비디오는 mp4, mov만 업로드 가능)
+        </p>
+      );
     }
   };
 
@@ -417,119 +426,135 @@ export default function ChattingForm() {
       <div className="-mx-[260px] bg-color-gray-50 py-6 mobile-tablet:mb-5 card:mb-5">
         <p className="semibold pl-[260px] text-xl">메시지</p>
       </div>
+
       <Layout bodyClass="bg-gray">
-        <div className="gap-4 overflow-x-auto rounded-xl border border-color-line-200 bg-color-gray-50 p-4 pc:hidden mobile-tablet:flex card:flex">
-          {chatRooms.length === 0 ? (
-            <p>채팅방 목록이 없습니다.</p>
-          ) : (
-            chatRooms.map((room) => (
-              <div
-                key={room.id}
-                onClick={() => handleChatRoomClick(room)}
-                className={`flex cursor-pointer flex-col rounded-lg p-3 ${selectedChatRoom?.id === room.id ? "bg-color-blue-100" : "bg-color-gray-50"}`}
-              >
-                <Image
-                  src={avatarImages.find((avatar) => avatar.key === room.users[1]?.image)?.src}
-                  alt="유저"
-                  width={70}
-                  className="rounded-full"
-                />
-                <p className="bold mt-3 text-center">{room.users[1]?.nickName}</p>
-              </div>
-            ))
-          )}
-        </div>
-        <div className="grid h-[920px] grid-cols-7 gap-10 pt-4 mobile-tablet:pt-5">
-          {/* 채팅방 목록 */}
-          <div className="col-span-2 flex flex-col gap-4 overflow-y-auto rounded-xl bg-color-gray-50 p-8 mobile-tablet:hidden card:hidden">
-            {chatRooms.length === 0 ? (
-              <p>채팅방 목록이 없습니다.</p>
-            ) : (
-              chatRooms.map((room) => (
-                <div
-                  key={room.id}
-                  onClick={() => handleChatRoomClick(room)}
-                  className={`flex cursor-pointer gap-4 rounded-xl border border-color-line-100 p-4 ${selectedChatRoom?.id === room.id ? "bg-color-blue-100" : "bg-white"}`}
-                >
-                  <div>
+        {isLoading ? (
+          <div className="flex h-screen items-center justify-center">
+            <Image src={loading} alt="로딩 중" />
+          </div>
+        ) : (
+          <>
+            <div className="gap-4 overflow-x-auto rounded-xl border border-color-line-200 bg-color-gray-50 p-4 pc:hidden mobile-tablet:flex card:flex">
+              {chatRooms.length === 0 ? (
+                <p>채팅방 목록이 없습니다.</p>
+              ) : (
+                chatRooms.map((room) => (
+                  <div
+                    key={room.id}
+                    onClick={() => handleChatRoomClick(room)}
+                    className={`flex cursor-pointer flex-col rounded-lg p-3 ${selectedChatRoom?.id === room.id ? "bg-color-blue-100" : "bg-color-gray-50"}`}
+                  >
                     <Image
                       src={avatarImages.find((avatar) => avatar.key === room.users[1]?.image)?.src}
                       alt="유저"
                       width={70}
                       className="rounded-full"
                     />
+                    <p className="bold mt-3 text-center">{room.users[1]?.nickName}</p>
                   </div>
-                  <div className="flex flex-col gap-3">
-                    <p>{room.users[1]?.nickName}</p>
-                    <p className="line-clamp-2">{room.lastChat}</p>
-                  </div>
-                </div>
-              ))
-            )}
-          </div>
-          {/* 채팅창 */}
-          <div className="col-span-5 rounded-xl bg-color-gray-50 p-8 mobile-tablet:col-span-7 card:col-span-7">
-            {selectedChatRoom === null ? (
-              ""
-            ) : (
-              <div className="mb-4 rounded-lg border border-color-line-100 p-4">
-                <p className="semibold text-2xl text-color-black-300 mobile-tablet:text-xl">
-                  {selectedChatRoom?.planTitle}
-                </p>
-                <div className="flex gap-4">
-                  <p className="regular text-xl text-color-gray-500 mobile-tablet:text-2lg">
-                    여행일
-                  </p>
-                  <p className="medium text-color-balck-400 text-xl mobile-tablet:text-2lg">
-                    {formatToDetailedDate(selectedChatRoom?.planTripDate || "")}
-                  </p>
-                  <div className="flex flex-row">
-                    <p className="regular text-xl text-color-gray-500 mobile-tablet:text-2lg">
-                      플랜가
+                ))
+              )}
+            </div>
+            <div className="grid h-[920px] grid-cols-7 gap-10 pt-4 mobile-tablet:pt-5">
+              {/* 채팅방 목록 */}
+              <div className="col-span-2 flex flex-col gap-4 overflow-y-auto rounded-xl bg-color-gray-50 p-8 mobile-tablet:hidden card:hidden">
+                {chatRooms.length === 0 ? (
+                  <p>채팅방 목록이 없습니다.</p>
+                ) : (
+                  chatRooms.map((room) => (
+                    <div
+                      key={room.id}
+                      onClick={() => handleChatRoomClick(room)}
+                      className={`flex cursor-pointer gap-4 rounded-xl border border-color-line-100 p-4 ${selectedChatRoom?.id === room.id ? "bg-color-blue-100" : "bg-white"}`}
+                    >
+                      <div>
+                        <Image
+                          src={
+                            avatarImages.find((avatar) => avatar.key === room.users[1]?.image)?.src
+                          }
+                          alt="유저"
+                          width={70}
+                          className="rounded-full"
+                        />
+                      </div>
+                      <div className="flex flex-col gap-3">
+                        <p>{room.users[1]?.nickName}</p>
+                        <p className="line-clamp-2">{room.lastChat}</p>
+                      </div>
+                    </div>
+                  ))
+                )}
+              </div>
+              {/* 채팅창 */}
+              <div className="col-span-5 rounded-xl bg-color-gray-50 p-8 mobile-tablet:col-span-7 card:col-span-7">
+                {selectedChatRoom === null ? (
+                  ""
+                ) : (
+                  <div className="mb-4 rounded-lg border border-color-line-100 p-4">
+                    <p className="semibold text-2xl text-color-black-300 mobile-tablet:text-xl">
+                      {selectedChatRoom?.planTitle}
                     </p>
-                    <Image src={coconut} alt="코코넛" width={30} />
+                    <div className="flex gap-4">
+                      <p className="regular text-xl text-color-gray-500 mobile-tablet:text-2lg">
+                        여행일
+                      </p>
+                      <p className="medium text-color-balck-400 text-xl mobile-tablet:text-2lg">
+                        {formatToDetailedDate(selectedChatRoom?.planTripDate || "")}
+                      </p>
+                      <div className="flex flex-row">
+                        <p className="regular text-xl text-color-gray-500 mobile-tablet:text-2lg">
+                          플랜가
+                        </p>
+                        <Image src={coconut} alt="코코넛" width={30} />
+                      </div>
+                      <p className="medium text-color-balck-400 text-xl mobile-tablet:text-2lg">
+                        {selectedChatRoom?.quotePrice} P
+                      </p>
+                    </div>
                   </div>
-                  <p className="medium text-color-balck-400 text-xl mobile-tablet:text-2lg">
-                    {selectedChatRoom?.quotePrice} P
-                  </p>
+                )}
+                <div
+                  className="h-[600px] overflow-y-auto mobile-tablet:h-[650px]"
+                  ref={messagesContainerRef}
+                >
+                  {renderMessages()}
+                </div>
+                <div className="flex flex-col gap-5" ref={messagesEndRef}>
+                  <input
+                    className="h-16 w-full rounded-xl bg-color-background-200 indent-5 text-color-black-500 outline-none mobile-tablet:h-10"
+                    placeholder="텍스트를 입력해 주세요."
+                    onChange={(e) => setMessage(e.target.value)}
+                    value={message}
+                    onKeyDown={handleKeyDown}
+                  />
+
+                  <div className="flex justify-between">
+                    <input
+                      type="file"
+                      className="hidden"
+                      id="fileUpload"
+                      onChange={handleFileChange}
+                    />
+                    <label
+                      htmlFor="fileUpload"
+                      className="cursor-pointer rounded-xl border border-color-blue-300 bg-color-blue-100 px-6 py-3 text-lg text-color-blue-300 mobile-tablet:px-4 mobile-tablet:py-1"
+                    >
+                      첨부파일
+                    </label>
+
+                    <button
+                      onClick={handleSendMessage}
+                      className="rounded-xl bg-color-blue-300 px-6 py-3 text-lg text-color-gray-50 mobile-tablet:px-4 mobile-tablet:py-1"
+                    >
+                      전송
+                    </button>
+                  </div>
+                  {filePreview && <div className="mb-3 mt-3">{renderFilePreview()}</div>}
                 </div>
               </div>
-            )}
-            <div
-              className="h-[600px] overflow-y-auto mobile-tablet:h-[650px]"
-              ref={messagesContainerRef}
-            >
-              {renderMessages()}
             </div>
-            <div className="flex flex-col gap-5" ref={messagesEndRef}>
-              <input
-                className="h-16 w-full rounded-xl bg-color-background-200 indent-5 text-color-black-500 outline-none mobile-tablet:h-10"
-                placeholder="텍스트를 입력해 주세요."
-                onChange={(e) => setMessage(e.target.value)}
-                value={message}
-                onKeyDown={handleKeyDown}
-              />
-
-              <div className="flex justify-between">
-                <input type="file" className="hidden" id="fileUpload" onChange={handleFileChange} />
-                <label
-                  htmlFor="fileUpload"
-                  className="cursor-pointer rounded-xl border border-color-blue-300 bg-color-blue-100 px-6 py-3 text-lg text-color-blue-300 mobile-tablet:px-4 mobile-tablet:py-1"
-                >
-                  첨부파일
-                </label>
-
-                <button
-                  onClick={handleSendMessage}
-                  className="rounded-xl bg-color-blue-300 px-6 py-3 text-lg text-color-gray-50 mobile-tablet:px-4 mobile-tablet:py-1"
-                >
-                  전송
-                </button>
-              </div>
-              {filePreview && <div className="mb-3 mt-3">{renderFilePreview()}</div>}
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </Layout>
     </>
   );
